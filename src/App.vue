dab6a21eb3a17301ae430e3cf1b32c7993221c43
Wladimir Guevara <wladige@gmail.com>
2020-09-05 21:23:03 -0400

    renderizando imagen

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 vue <template>
   <div>
-    
+      <div>
+        <input type="text" placeholder="mensaje" v-model="mensaje">
+    <p>Filtro
+      <select name="" id=""></select>
+    </p>
+
+    <p>
+      Color
+      <select name="color" id=""></select>
+    </p>
+    <button>buscar mininos</button>
+      </div>
+      <img :src="gatito"  alt="gatos">
   </div>
 </template>
 
 <script>
 export default {
-  
+  name: "app",
+  data() {
+    return {
+      mensaje:""
+    }
+  },
+  computed: {
+    gatito(){
+      return `https://cataas.com/cat/says/${this.mensaje}`;
+    },
+   
+  },
 }
 </script>
 
-<style lang="stylus" scoped>
+<style lang="scss" scoped>
 
 </style>
