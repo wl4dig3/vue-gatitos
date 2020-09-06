746b3331b3da323c645e10ddff6351ecc3f7c3ee
Wladimir Guevara <wladige@gmail.com>
2020-09-05 22:50:16 -0400

    se agrega filtro en computed, e input

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 vue <template>
   <div>
-      <div>
-        <input type="text" placeholder="mensaje" v-model="mensaje">
-    <p>Filtro
-      <select name="" id=""></select>
-    </p>
+    <div class="container">
+      <h3 class="text-center mt-5">Ingresa un mensaje en el campo de texto y presiona 'enter'</h3>
+      <div class="input-wrap">
+        <input class="mt-3" type="text" placeholder="mensaje" v-model="mensaje" />
+        <p>
+          Filtro
+          <select v-model="filterSlected">
+            <option disabled selected>Seleccione:</option>
+            <option
+              @click="filterSelected = item"
+              v-for="item in filter"
+              :key="item.id"
+              :value="item"
+            >{{item}}</option>
+          </select>
+        </p>
 
-    <p>
-      Color
-      <select name="color" id=""></select>
-    </p>
-    <button>buscar mininos</button>
+        <p>
+          Color
+          <select name="color" id></select>
+        </p>
       </div>
-      <img :src="gatito"  alt="gatos">
+      <!-- <button>buscar mininos</button> -->
+    </div>
+    <img :src="gatito" alt />
   </div>
 </template>
 
@@ -21,18 +33,22 @@ export default {
   name: "app",
   data() {
     return {
-      mensaje:""
-    }
+      filter: ["blur", "mono", "sepia", "negative", "paint", "pixel"],
+      color: ["red", "green", "yellow", "blue", "black", "gray"],
+      filterSelected: "Selecciona:",
+      mensaje: "",
+    };
   },
   computed: {
-    gatito(){
-      return `https://cataas.com/cat/says/${this.mensaje}`;
+    gatito() {
+      return `https://cataas.com/cat/says/${this.mensaje}?filter=${this.filterSelected}`;
     },
-   
   },
-}
+};
 </script>
 
 <style lang="scss" scoped>
-
+p {
+  margin: 1rem 0;
+}
 </style>
