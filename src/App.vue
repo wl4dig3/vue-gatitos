b4f67974d47b2c8f2745fa3678620855d176a1fa
Wladimir Guevara <wladige@gmail.com>
2020-09-07 23:29:05 -0300

    probando cambios selector color

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,82 @@
 vue <template>
-  <div>
+<div>
     <h3 class="text-center mt-5">Ingresa un titulo para el gatito y presiona 'enter'</h3>
     <div class="container">
-      <div class="input-wrap">
-        <input class="mt-3" type="text" placeholder="titulo" v-model="mensaje" />
-        <p>
-          Filtro
-          <select v-model="filterSelected">
-            <option disabled selected>Seleccione:</option>
-            <option
-              @click="filterSelected = item"
-              v-for="item in filter"
-              :key="item.id"
-              :value="item"
-            >{{item}}</option>
-          </select>
-        </p>
+        <div class="input-wrap">
+            <input class="mt-3" type="text" placeholder="titulo" v-model="mensaje" />
+            <p>
+                Filtro
+                <select v-model="filterSelected">
+                    <option disabled selected>Seleccione:</option>
+                    <option @click="filterSelected = item" v-for="item in filter" :key="item.id" :value="item">{{item}}</option>
+                </select>
+            </p>
 
-        <p>
-          Color
-          <select v-model="colorSelected">
-            <option disabled selected>Selecciona:</option>
-            <option
-              @click="colorSelecteed = item"
-              v-for="item in colors"
-              :key="item.id"
-              :value="item"
-            >{{item}}</option>
-          </select>
-        </p>
-        <div :style="{ 'background-color': this.colorSelected}"
-        class="color-round"
-        >
+            <p>
+                Color
+                <select v-model="colorSelected">
+                    <option disabled selected>Selecciona:</option>
+                    <option @click="colorSelecteed = item" v-for="item in colors" :key="item.id" :value="item">{{item}}</option>
+                </select>
+            </p>
+            <div class="colorRound" :style="{ 'backgroundColor': this.colorSelected}">
 
+            </div>
+
+            <p>
+                Tamaño:
+                <input type="number" v-model.number="size" min="1" />
+            </p>
         </div>
-
-        <p>
-          Tamaño:
-          <input type="number" v-model.number="size" min="1" />
-        </p>
-      </div>
-      <!-- <button>buscar mininos</button> -->
+        <!-- <button>buscar mininos</button> -->
     </div>
-    <img :src="gatito" alt />
-  </div>
+    <img :src="gatito" alt="tu gatito aqui" />
+</div>
 </template>
 
 <script>
 export default {
-  name: "app",
-  data() {
-    return {
-      filter: ["blur", "mono", "sepia", "negative", "paint", "pixel"],
-      colors: ["red", "green", "yellow", "blue", "black", "gray"],
-      filterSelected: "Selecciona:",
-      colorSelected: "Selecciones",
-      size: null,
-      mensaje: "",
-    };
-  },
-  computed: {
-    gatito() {
-      return `https://cataas.com/cat/says/${this.mensaje}?filter=${this.filterSelected}&color=${this.colorSelected}&size=${this.size}`;
+    name: "app",
+    data() {
+        return {
+            filter: ["blur", "mono", "sepia", "negative", "paint", "pixel"],
+            colors: ["red", "green", "yellow", "blue", "black", "gray"],
+            filterSelected: "Selecciona un filtro:",
+            colorSelected: "Selecciona un color",
+            // size: null,
+            mensaje: "",
+        };
     },
-  },
+    computed: {
+        gatito() {
+            return `https://cataas.com/cat/says/${this.mensaje}?filter=${this.filterSelected}&color=${this.colorSelected}&size=${this.size}`;
+        },
+    },
 };
 </script>
 
 <style lang="scss" scoped>
-
 .container {
-  background-color: pink;
-  margin: 2rem auto;
-  padding: 1rem;
+    background-color: pink;
+    margin: 2rem auto;
+    padding: 1rem;
 }
+
 p {
-  margin: 1rem 0;
+    margin: 1rem 0;
 }
+
 img {
-  border-radius: 50%;
-  margin: 1rem 0 0 20rem;
+    border-radius: 50%;
+    margin: 1rem 0 0 20rem;
 }
-color-round{
-  border-radius: 50%;
-  width: 3rem;
-  height: 3rem;
-  margin-left: 2rem;;
+
+colorRound {
+    border-radius: 50%;
+    width: 2rem;
+    height: 2rem;
+    margin-left: 2rem;
+
 }
 </style> 
 
